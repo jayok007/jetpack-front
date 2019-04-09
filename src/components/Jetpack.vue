9da3fa9451b621486e85a8f498f23d0f50ca623f
Swann CASTEL <swann.castel@hotmail.fr>
2019-04-09 15:32:01 +0200

    feat: add basic jetpacks list

---
+++ src/components/Jetpack.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-card>
+    <v-img :src="image" aspect-ratio="1" height="100"></v-img>
+
+    <v-card-title primary-title>
+      <div>
+        <h3 class="headline mb-0">{{ name }}</h3>
+      </div>
+    </v-card-title>
+
+    <v-card-actions>
+      <v-btn flat color="orange">Share</v-btn>
+      <v-btn flat color="orange">Explore</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'Jetpack',
+
+  props: {
+    id: String,
+    name: String,
+    image: String
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,47 @@
 <template>
-  <HelloWorld />
+  <div>
+    <jetpack
+      v-for="(jetpack, i) in jetpacks"
+      :key="i"
+      :id="jetpack.id"
+      :name="jetpack.name"
+      :image="jetpack.image"
+    ></jetpack>
+
+    <v-text-field type="text" v-model="text"></v-text-field>
+    {{ text }}
+    <v-btn @focus="validate">Valider</v-btn>
+  </div>
 </template>
 
 <script>
-import HelloWorld from '../components/HelloWorld'
+import Jetpack from '../components/Jetpack'
 
 export default {
+  name: 'Home',
+
   components: {
-    HelloWorld
+    Jetpack
+  },
+
+  data() {
+    return {
+      jetpacks: [],
+      text: 'toto'
+    }
+  },
+
+  created() {
+    window
+      .fetch('/jetpacks')
+      .then(res => res.json())
+      .then(jetpacks => (this.jetpacks = jetpacks))
+  },
+
+  methods: {
+    validate() {
+      console.log(this.text)
+    }
   }
 }
 </script>
