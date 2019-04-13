a7ac9cc6ecb7683a284598d172362c819b2c87f3
Swann CASTEL <swann.castel@hotmail.fr>
2019-04-13 20:03:20 +0200

    chore(vuex): remove pointless store

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
-import store from './store'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
@@ -10,6 +9,5 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
-  store,
   render: h => h(App)
 }).$mount('#app')
