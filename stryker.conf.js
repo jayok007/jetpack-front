f1362c6196e1e037c4e3b09e6b85d119a99af9bd
Swann CASTEL <swann.castel@hotmail.fr>
2019-04-13 20:22:04 +0200

    chore(stryker): add threshold

--- stryker.conf.js
+++ stryker.conf.js
@@ -1,5 +1,3 @@
-// This config was generated using a preset.
-// Please see the handbook for more information: https://github.com/stryker-mutator/stryker-handbook/blob/master/stryker/guides/vuejs.md#vuejs
 module.exports = config => {
   config.set({
     mutate: ['src/httpClient.js', 'src/**/*.vue'],
@@ -7,6 +5,9 @@ module.exports = config => {
     coverageAnalysis: 'off',
     commandRunner: {
       command: 'npx vue-cli-service test:unit'
+    },
+    thresholds: {
+      break: 70
     }
   })
 }
