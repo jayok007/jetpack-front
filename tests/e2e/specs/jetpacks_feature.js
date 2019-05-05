5d63a1b392a673bc760b3b901b4dc4ee6e018124
Swann CASTEL <swann.castel@hotmail.fr>
2019-05-05 22:10:20 +0200

    test(Jetpack): add update e2e test

--- tests/e2e/specs/jetpacks_feature.js
+++ tests/e2e/specs/jetpacks_feature.js
@@ -11,11 +11,35 @@ describe('Jetpacks', () => {
 
   it('should add a jetpack', () => {
     cy.get('[data-test="createJetpackBtn"]').click()
-    cy.get('[data-test="nameInput"]').type('Jetpack Joyride')
-    cy.uploadImage('jetpack-4.png', '[data-test="imageInput"]')
 
-    cy.get('[data-test="saveBtn"]').click()
+    cy.get('[data-test="createJetpackForm"]').within(() => {
+      cy.get('[data-test="nameInput"]').type('Jetpack Joyride')
+
+      cy.uploadImage('jetpack-4.png', '[data-test="imageInput"]')
+
+      cy.get('[data-test="saveBtn"]').click()
+    })
 
     cy.get('[data-test="jetpack"]').should('contain', 'Jetpack Joyride')
+  })
+
+  it('should update a jetpack', () => {
+    cy.get('[data-test="updateBtn"]')
+      .first()
+      .click()
+
+    cy.get('[data-test="updateForm"]')
+      .last()
+      .within(() => {
+        cy.get('[data-test="nameInput"]')
+          .clear()
+          .type('Updated')
+
+        cy.get('[data-test="saveBtn"]').click()
+      })
+
+    cy.get('[data-test="jetpack"]')
+      .should('contain', 'Updated')
+      .and('not.contain', 'Jetpack Fortnite Wiki')
   })
 })
