67bba3d9e2e460ae9d70f80d50851c9357f82528
Samir <samir.lazzali@gmail.com>
2019-05-06 14:04:29 +0200

    Add unit test for search

--- tests/unit/jetpack/Search.spec.js
+++ tests/unit/jetpack/Search.spec.js
@@ -33,8 +33,30 @@ describe('Search.vue', () => {
         dateEnd +
         '"'
     )
-
     const result = wrapper.find('[data-test="searchResult"]')
     expect(result.isVisible()).toBe(true)
+  })
+  it('should display the book a jetpack ', async () => {
+    const dateStart = new Date().toISOString().substr(0, 10)
+    const dateEnd = new Date().toISOString().substr(0, 10)
+
+    wrapper.find('[data-test="searchBtn"]').trigger('click')
+    await wrapper.vm.$nextTick()
+
+    expect(httpClient.get).toHaveBeenCalledWith(
+      '/api/availability/jetpacks?dateStart="' +
+        dateStart +
+        '"&dateEnd="' +
+        dateEnd +
+        '"'
+    )
+
+    wrapper.find('[data-test="bookingBtn"]').trigger('click')
+
+    const newSearchBeforeQuery = wrapper.find('[data-test="newSearch"]')
+    expect(newSearchBeforeQuery.isVisible()).toBe(true)
+
+    const resultBeforeQuery = wrapper.find('[data-test="searchResult"]')
+    expect(resultBeforeQuery.isVisible()).toBe(true)
   })
 })
